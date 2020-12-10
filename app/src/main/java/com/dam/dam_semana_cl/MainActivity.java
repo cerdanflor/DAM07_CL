f4daeb4a2370925d5aef8807e48c339d967f7152
Flor Cerdán <cerdanflor@gmail.com>
2020-12-10 14:12:50 -0500

    Iniciando DAM_Semana07 Constraint Layout

---
+++ app/src/main/java/com/dam/dam_semana_cl/MainActivity.java
@@ -0,0 +1,14 @@
+package com.dam.dam_semana_cl;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
