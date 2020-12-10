ec5b2ac716dc0c7c6f535b1bee329de1f64e635d
Flor Cerdán <cerdanflor@gmail.com>
2020-12-10 16:41:17 -0500

    Fragment List

--- app/src/main/java/com/dam/dam_semana_cl/MainActivity.java
+++ app/src/main/java/com/dam/dam_semana_cl/MainActivity.java
@@ -2,13 +2,24 @@ package com.dam.dam_semana_cl;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btnLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnLogin = findViewById(R.id.buttonLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, NotasActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
